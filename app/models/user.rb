5f39fa81ebd8aab2dd4b15316ebfc35fb89a70c3
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-06-09 11:24:11 +0900

    ユーザー管理 修正#1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
 
-  # validates :email, unique: true あとで聞く場所
-
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-
+with_options presence: true do
+  validates :nickname
+  validates :birthday
   validates :password, length: {minimum:6}, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+  
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name
+  end
 
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' }
+  with_options format:{  with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'} do
 
-  validates :last_name_reading, presence: true, format:{  with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
-  validates :first_name_reading, presence: true, format:{  with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'}
+    validates :last_name_reading
+    validates :first_name_reading
+  end
+ end
 end
