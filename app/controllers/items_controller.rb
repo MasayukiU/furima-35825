eedb7b027fad92918deb19143351e58c7f843624
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-06-10 15:45:40 +0900

    商品出品 コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
 	def index
 	end
+
+  def new 
+    @item = Item.new
+  end
+
+  def create 
+	  @item = Item.new(item_params) 
+		if @item.save
+      redirect_to items_path(@items)
+    else
+      render :new
+	  end
+  end
+
+	private
+
+  def item_params
+    params.require(:item).permit(:item_name, :price, :product_description, 
+      :category_id, :item_condition_id, :shipping_charge_id, :shipping_area_id, 
+      :estimated_time_id, :image).merge(user_id: current_user.id)
+  end
 end
