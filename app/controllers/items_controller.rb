e38721b8d5c8b10243652b9fa4e83f35cf5d9b64
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-06-11 19:04:07 +0900

    ルボコップ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,30 @@
 class ItemsController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :show]
-	before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
 
-	def index
-    @items = Item.all.order("created_at DESC")
-	end
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
 
-  def new 
+  def new
     @item = Item.new
   end
 
-  def create 
-	  @item = Item.new(item_params) 
-		if @item.save
+  def create
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to items_path
     else
       render :new
-	  end
+    end
   end
 
   def show
   end
 
   def edit
-	end
+  end
 
   def update
     if @item.update(item_params)
@@ -34,19 +34,17 @@ class ItemsController < ApplicationController
     end
   end
 
-	private
+  private
 
   def item_params
-    params.require(:item).permit(:item_name, :price, :product_description, 
-      :category_id, :item_condition_id, :shipping_charge_id, :shipping_area_id, 
-      :estimated_time_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :price, :product_description,
+                                 :category_id, :item_condition_id, :shipping_charge_id, :shipping_area_id,
+                                 :estimated_time_id, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
-     		redirect_to action: :index
-     end
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def set_item
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :items
 
-with_options presence: true do
-  validates :nickname
-  validates :birthday
-  validates :password, length: {minimum:6}, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
-  
-  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
-    validates :last_name
-    validates :first_name
-  end
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password, length: { minimum: 6 },
+                         format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 
-  with_options format:{  with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters'} do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
 
-    validates :last_name_reading
-    validates :first_name_reading
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'Input full-width katakana characters' } do
+      validates :last_name_reading
+      validates :first_name_reading
+    end
   end
- end
 end
