96166956bef3188259aa2ca8afc52e0cc94b56d0
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-06-16 01:08:10 +0900

    商品購入機能 コントローラー

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
+
+
+  def index
+    @order_address = OrderAddress.new
+    if current_user == @item.user
+       redirect_to root_path
+    end
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      pay_item
+       @order_address.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :shipping_area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token], #ストロングパラメーター内のtokenを指定
+      currency: 'jpy' 
+    )
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    unless current_user.id || @item.user_id && @item.order.present?
+      return redirect_to root_path 
+    end
+  end
+end
