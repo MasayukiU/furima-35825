543efff8cd2e04841aa1be5c9a002361a4375e22
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-07-11 00:45:18 +0900

    タグ付け Formオブジェクト

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,19 +10,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
 
-  with_options presence: true do
-    # validates :images
-    validates :item_name
-    validates :product_description
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Please enter half-width' },
-                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
-
-    with_options numericality: { other_than: 0 } do
-      validates :category_id
-      validates :estimated_time_id
-      validates :item_condition_id
-      validates :shipping_area_id
-      validates :shipping_charge_id
-    end
-  end
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
 end
